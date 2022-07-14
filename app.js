9c74235bde4e8b39e42e2e6f62895abeea411fed
giusepasian <83140249+giusepasian@users.noreply.github.com>
2022-07-14 09:19:39 -0300

    Agregando home.html

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const pathPublic = path.resolve(__dirname, './public');
+app.use(express.static(pathPublic));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen (8080, () => {
+    console.log('Servidor iniciado en: http://localhost:8080');
+});
+
