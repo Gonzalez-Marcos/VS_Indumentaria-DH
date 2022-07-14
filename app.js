d152fa7edec85ab2a914579c9513de9ee2fbd595
BARBARA RIOS <gonzalezmarcos_90@hotmail.com>
2022-07-14 20:53:15 -0300

    Modifique app

--- app.js
+++ app.js
@@ -10,33 +10,20 @@ app.use(express.static(pathPublic));
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname, '/views/home.html');
 });
 
 app.get('/productDetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
+    res.sendFile(__dirname, '/views/productDetail.html');
 });
 app.get('/productCart', (req,res)=>{
-    res.sendFile(__dirname + '/views/productCart.html');
+    res.sendFile(__dirname, '/views/productCart.html');
 });
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname, '/views/register.html');
 });
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-
-app.get('/product', (req,res)=>{
-    res.sendFile(__dirname + '/views/productCard.html');
-});
-
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname, '/views/login.html');
 });
 
 app.listen (8080, () => {
