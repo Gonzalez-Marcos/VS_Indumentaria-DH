d4a4019aaa48cadc680a4df68ab88565ec437404
Marcos Gonzalez <gonzalezmarcos_90@hotmail.com>
2022-07-22 21:20:58 -0300

    Modicamos rutas y controller

--- src/routers/products.js
+++ src/routers/products.js
@@ -6,8 +6,8 @@ const productDetailController = require('../controllers/productDetailController'
 
 const router = express.Router();
 
-router.get('/', productCartController.productCart);
+router.get('/productCart', productCartController.productCart);
 
-router.get('/', productDetailController.productDetail);
+router.get('/productDetail', productDetailController.productDetail);
 
 module.exports = router;
