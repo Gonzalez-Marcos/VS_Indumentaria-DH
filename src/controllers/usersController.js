6cc392eed7f433f129db169e19c1e6abfb515b66
giusepasian <83140249+giusepasian@users.noreply.github.com>
2022-08-16 10:10:33 -0300

    primera parte de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,7 @@
-const { read } = require('fs');
+const { read, fstat } = require('fs');
 const path = require('path');
+
+const users = [];
 
 const controller = {
     login: (req, res) => {
@@ -11,6 +13,21 @@ const controller = {
     register: (req, res) => {
         res.render('users/register');
     },
+    createUsers: (req, res) => {
+        const {name, last_name} = req.body;
+        if (!name || !last_name){
+            res.status(400).send('Escribir todos los campos obligatorios')
+            return;
+        }
+        let newUser = {
+            name,
+            last_name
+        };
+        users.push(newUser);
+        const json_users = JSON.stringify(users)
+        fs.writefileSync('src/database/users.json', json_users, 'utf-8')
+        res.send('received');
+    }
 };
 
 module.exports = controller;
--- src/routers/users.js
+++ src/routers/users.js
@@ -7,25 +7,17 @@ const { body } = require('express-validator') //Libreria de Validacion
 //const loginController = require('../controllers/loginController');
 const usersController = require('../controllers/usersController');
 //const registerController = require('../controllers/registerController');
-
 const passwController = require('../controllers/passwController')
-
 const logMiddleWare = require('../middleWares/logDBMiddleWare') //Ruta de middleWares para DB
 
 
 //rutas
 
-//router.get('/login', loginController.login);
 router.get('/login', usersController.login);
-
-
 router.post('/', logMiddleWare, usersController.logged);
-
-//router.get('/register', registerController.register);
 router.get('/register', usersController.register);
-
+router.post('/register', usersController.createUsers);
 router.get('/pass', passwController.passw);
-
 router.post('/login', passwController.recPassw);
 
 module.exports = router;
