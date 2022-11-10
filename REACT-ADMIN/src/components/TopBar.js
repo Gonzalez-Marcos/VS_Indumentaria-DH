a75584de0bfd94f395113f6ef1b81e6a5d6cfd96
giuseppe <giusepasian@gmail.com>
2022-11-10 18:12:27 -0300

    modificaciones

--- REACT-ADMIN/src/components/TopBar.js
+++ REACT-ADMIN/src/components/TopBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import foto from '../assets/images/jordan-walke.png';
+import foto from '../assets/images/foto-perfil.jpg';
 
 function TopBar(){
     return(
@@ -38,7 +38,7 @@ function TopBar(){
 						{/*<!-- Nav Item - User Information -->*/}
 						<li className="nav-item dropdown no-arrow">
 							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Jordan Walke</span>
+								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Giuse</span>
 								<img className="img-profile rounded-circle" src={foto} alt="Jordan Walke - Creador de React" width="60"/>
 							</a>
 						</li>
